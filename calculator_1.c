164874a7582a756bf69ad7b41cb2126450d123f4
geun9716 <crintop@gmail.com>
2016-06-10 01:51:40 +0900

    Update calculator_1.c

--- calculator_1.c
+++ calculator_1.c
@@ -3,14 +3,23 @@
 #include <string.h>
 char plus(char a[60], char b[60])
 {
-	char result[61];
-	for (int i=60; i<1; i--)
+	char a[63]="1234124125135123146";
+	char b[63]="9999123412351252213";
+	char c[63], d[63], result[64];
+	int len, i;
+	
+	array(a, c);
+	array(b, d);
+	for (i = 63; i >= 0;i--)
 	{
-		result[i+1]=(a[i]-48)+(b[i]-48);
-		if (result[i+1]>10)
-			result[i] += 1;
+		result[i] = (c[i]-'0')+(d[i]-'0');
+		if (result[i] >= 9)
+		{
+			c[i-1] += 1;
+			result[i] = (result[i] - 10) + '0'; 
+		}
 	}
-	return result[61];
+	// 계산이 일정부분 되다가 마는데 i에서 꼬인것 같은데 잘 못하겠음 HELP
 }
 char minus(char a[60], char b[60])
 {
